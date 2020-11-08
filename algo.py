7e92cf6f908f597b68a4b47493297ca182ccbc1a
sgi <69767184+sgilavendano@users.noreply.github.com>
2020-11-07 23:42:21 -0500

    Update algo.py

--- algo.py
+++ algo.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import scipy
 from bs4 import BeautifulSoup
-#from wallstreet import Stock, Call, Put
 import quandl
+quandl.ApiConfig.api_key = 'z8yaUmivVu3o-3KngVCH'
+#from wallstreet import Stock, Call, Put
 #s = Stock('AAPL')
 #print(s.price)
 
