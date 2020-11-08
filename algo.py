4f050f77c46628be2714a0d1608d711481c42fe0
suryanshpsingh <67669995+suryanshpsingh@users.noreply.github.com>
2020-11-07 23:34:34 -0500

    Import quandl

--- algo.py
+++ algo.py
@@ -1,9 +1,9 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
+import scipy
 from bs4 import BeautifulSoup
-from wallstreet import Stock, Call, Put
-
-s = Stock('AAPL')
-
-print(s.price)
+#from wallstreet import Stock, Call, Put
+import quandl
+#s = Stock('AAPL')
+#print(s.price)
 
