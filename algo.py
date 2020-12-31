c2488b100a9ad02b8078e2cc606251d7dd1b6305
sgi <69767184+sgilavendano@users.noreply.github.com>
2020-12-31 18:34:30 -0500

    Update algo.py

--- algo.py
+++ algo.py
@@ -3,8 +3,9 @@ import numpy as np
 import scipy
 from bs4 import BeautifulSoup
 import quandl
+
 quandl.ApiConfig.api_key = 'z8yaUmivVu3o-3KngVCH'
-#from wallstreet import Stock, Call, Put
-#s = Stock('AAPL')
-#print(s.price)
+
+mydata = quandl.get("EIA/PET_RWTC_D")
+
 
