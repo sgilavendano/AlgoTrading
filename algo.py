e8c7d2d69483f98bdd12d5a68b156ea4a3cfa7ed
suryanshpsingh <67669995+suryanshpsingh@users.noreply.github.com>
2021-01-12 21:57:48 -0500

    Update algo.py
    Import statements

--- algo.py
+++ algo.py
@@ -1,10 +1,11 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib as plt
 import numpy as np
-import scipy
-from bs4 import BeautifulSoup
+import statistics
+import math
 import quandl
-
 quandl.ApiConfig.api_key = 'z8yaUmivVu3o-3KngVCH'
+
 
 mydata = quandl.get("EIA/PET_RWTC_D")
 print(mydata)
