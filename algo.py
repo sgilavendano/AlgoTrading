37df89da36d8ee54861b2c90bfb645cbfd52aa84
sgi <69767184+sgilavendano@users.noreply.github.com>
2021-01-23 21:22:13 -0500

    Update algo.py

--- algo.py
+++ algo.py
@@ -43,6 +43,50 @@ def get_macd():
   for x in range(0,len(ema20)):
     macd[x] = macd[x] - ema20[x]
   plt.pyplot.plot(x1,macd)
+def get_bollingerbands():
+  bol_data = quandl.get("EOD/AAPL", returns= "numpy")
+  upper_bollingerband=[]
+  lower_bollingerband=[]
+  middlebollinger=[]
+  xcord=[]
+  ycord=[]
+
+  for data in bol_data:
+    xcord.append(data[0])
+    ycord.append(data[1])
+  
+  #creating a tuple from the data
+  xcord=xcord[19:]
+  for a in range(19,len(ycord)):
+    avg = (ycord[a] + ycord[a-19] + ycord[a-18] + ycord[a-17] + ycord[a-16]+ ycord[a-15] + ycord[a-14] + ycord[a-13] + ycord[a-12] + ycord[a-11]+ycord[a-10] + ycord[a-9] + ycord[a-8] + ycord[a-7] + ycord[a-6]+ ycord[a-5] + ycord[a-4] + ycord[a-3] + ycord[a-2] + ycord[a-1])/20
+    middlebollinger.append(avg)
+    templist= [ycord[a], ycord[a-19], ycord[a-18], ycord[a-17], ycord[a-16], ycord[a-15], ycord[a-14], ycord[a-13], ycord[a-12], ycord[a-11],ycord[a-10], ycord[a-9], ycord[a-8], ycord[a-7], ycord[a-6], ycord[a-5], ycord[a-4], ycord[a-3],ycord[a-2], ycord[a-1]]
+    std = np.std(templist)
+    #these if statements are not needed, but through seeing the data, i started noticing that 10-19 is the avg std and anything over and lower is extremely different
+    if(std> 20 ):
+      print("this is an extremely volatile time to invest in")
+    if(std<10):
+      print("At this point, there is little volatility")
+    upper_bollingerband.append( avg + (std*2))
+    lower_bollingerband.append( avg - (std*2))
+
+
+
+
+    #upper_bollingerband = middlebollinger + np.std(middlebollinger.std *2)
+    #lower_bollingerband = middlebollinger - np.std(middlebollinger.std *2)
+  
+  #I just gotta fix how to make the standarddev of the upper and lower band 
+  #Almost done
+  plt.pyplot.plot(xcord, middlebollinger)
+  plt.pyplot.plot(xcord,upper_bollingerband)
+  plt.pyplot.plot(xcord,lower_bollingerband)
+  
+
+
+
+  #print(bol_data)
+  #print(bolpanddata)
   
 def get_rsi():
   mydata = quandl.get("EIA/PET_RWTC_D", returns = "numpy")   #Crude oil
